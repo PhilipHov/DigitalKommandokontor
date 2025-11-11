d674c119c93b43f1c92cbcf20c82184687114928
PhilipHovgaard <philiphovgaard@users.noreply.github.com>
2025-11-11 17:05:35 +0100

    fix: set base path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const base = process.env.NODE_ENV === "production" ? "/DigitalKommandokontor/" : "/";
+
 export default defineConfig({
   plugins: [react()],
+  base,
   server: { port: 5173, host: "127.0.0.1" },
 });
